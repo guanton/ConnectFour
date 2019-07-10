8ab1986f9e708b1ff8f31f3b865bf049b2bcb4df
Vince <vincentguan23@gmail.com>
2019-07-09 17:05:10 -0700

    beats random player 100% of the time

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
     public static void main(String[] args) {
 
-        CFPlayer ai1 = new myAI();
+        CFPlayer ai1 = new RandomAI();
         CFPlayer ai2 = new minimaxAI();
         int n = 100;
         int winCount = 0;
@@ -10,7 +10,9 @@ public class Test {
             ConsoleCF game = new ConsoleCF(ai1, ai2); game.playOut();
             if (game.getWinner()==ai2.getName())
                 winCount++; }
-        System.out.print(ai2.getName() + " beats " + ai1.getName()); System.out.print(((double) winCount)/((double) n)*100 + "%"); System.out.print(" of the time.");
+        System.out.print(ai2.getName() + " beats " + ai1.getName()+ " ");
+        System.out.print(((double) winCount)/((double) n)*100 + "%");
+        System.out.print(" of the time.");
     }
 
 
